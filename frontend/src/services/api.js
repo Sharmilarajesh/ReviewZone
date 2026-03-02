229ecdd4dc06c55dad58a79a4a134875914b8099
Sharmila <sharmila210604@gmail.com>
2026-03-02 12:50:03 +0530

    Update api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.MODE === 'development' 
-  ? 'http://localhost:5000/api' 
-  : '/api';  
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 
 const API = axios.create({
   baseURL: baseURL
